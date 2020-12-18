addc730cb7f4135ca6f5ed329f4b19840a901d31
Berke Aras <info@berkearas.de>
2020-12-18 09:55:32 +0100

    chore(repository): Run prettier

--- src/App.js
+++ src/App.js
@@ -1,55 +1,46 @@
 import React, { Component } from 'react'
-import './scss/style.scss';
-import firebase from "firebase";
-import {
-	BrowserRouter as Router,
-	Switch,
-	Route,
-	Link
-} from "react-router-dom";
-import SignIn from "./views/auth/SignIn/";
-import SignUp from "./views/auth/SignUp/";
-import ProfilePage from "./views/auth/ProfilePage/";
-import PasswordReset from "./views/auth/PasswordReset/";
-import MainApp from "./views/App/";
-
+import './scss/style.scss'
+import firebase from 'firebase'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import SignIn from './views/auth/SignIn/'
+import SignUp from './views/auth/SignUp/'
+import ProfilePage from './views/auth/ProfilePage/'
+import PasswordReset from './views/auth/PasswordReset/'
+import MainApp from './views/App/'
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
+    constructor(props) {
+        super(props)
 
-		this.state = {
-			isLoggedIn: false,
-		}
-	}
+        this.state = {
+            isLoggedIn: false,
+        }
+    }
 
-	componentDidMount() {
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ isLoggedIn: true })
+            }
+        })
+    }
 
-		firebase.auth().onAuthStateChanged((user) => {
-			if (user) {
-				this.setState({isLoggedIn: true});
-			}
-		});
-
-	}
-
-	render() {
-		return (
-			<Router>
-				<Switch>
-
-					<Route path="/app">
-						<MainApp />
-					</Route>
-					<Route path="/signup">
-						<SignUp />
-					</Route>
-					<Route exact path="/">
-						<SignIn />
-					</Route>
-				</Switch>
-			</Router>
-		);
-	}
-};
-export default App;
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route path="/app">
+                        <MainApp />
+                    </Route>
+                    <Route path="/signup">
+                        <SignUp />
+                    </Route>
+                    <Route exact path="/">
+                        <SignIn />
+                    </Route>
+                </Switch>
+            </Router>
+        )
+    }
+}
+export default App
--- src/views/App/index.js
+++ src/views/App/index.js
@@ -1,38 +1,37 @@
 /* eslint-disable no-useless-constructor */
-import React, {useState} from "react";
+import React, { useState } from 'react'
 import {
-	BrowserRouter as Router,
-	Switch,
-	Route,
-	Link,
-	Redirect
-} from "react-router-dom";
-import './style.scss';
-import logo from '../../static/logo.svg';
-import { Button, Input } from 'semantic-ui-react';
-import firebase from 'firebase';
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect,
+} from 'react-router-dom'
+import './style.scss'
+import logo from '../../static/logo.svg'
+import { Button, Input } from 'semantic-ui-react'
+import firebase from 'firebase'
 
 // Components
-import Sidebar from '../../components/Sidebar/';
+import Sidebar from '../../components/Sidebar/'
 
 class MainApp extends React.Component {
-	constructor(props) {
-		super(props);
+    constructor(props) {
+        super(props)
 
-		firebase.auth().onAuthStateChanged((user) => {
-			if (!user) {
-				window.location.href = '/';
-			}
-		});
+        firebase.auth().onAuthStateChanged((user) => {
+            if (!user) {
+                window.location.href = '/'
+            }
+        })
+    }
 
-	}
-
-	render() {
-		return (
-			<div className="app">
-				<Sidebar></Sidebar>
-			</div>
-		);
-	}
-};
-export default MainApp;
+    render() {
+        return (
+            <div className="app">
+                <Sidebar></Sidebar>
+            </div>
+        )
+    }
+}
+export default MainApp
