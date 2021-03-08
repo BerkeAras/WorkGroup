6363ddd6c06b037e353c7a778bcdff9aa1feb5cc
Berke Aras <info@berkearas.de>
2021-03-08 11:00:54 +0100

    Run prettier

--- src/components/_App_PostList/index.jsx
+++ src/components/_App_PostList/index.jsx
@@ -146,51 +146,51 @@ class PostsList extends React.Component {
     toggleLike(e) {
         e.preventDefault()
 
-        let element;
-        let hasLiked = false;
-        let newLikeCount;
-        let currentLikeCount;
-        let postId;
-        let newLikeText;
+        let element
+        let hasLiked = false
+        let newLikeCount
+        let currentLikeCount
+        let postId
+        let newLikeText
 
-        if (e.target.tagName.toLowerCase() === "i" || e.target.tagName.toLowerCase() === "span") {
-            element = e.target.parentNode;
+        if (e.target.tagName.toLowerCase() === 'i' || e.target.tagName.toLowerCase() === 'span') {
+            element = e.target.parentNode
         } else {
-            element = e.target;
+            element = e.target
         }
 
         // Has liked
-        hasLiked = element.classList.contains("liked");
+        hasLiked = element.classList.contains('liked')
 
         // Update counter
-        currentLikeCount = element.querySelector('span').innerHTML.substr(0, element.querySelector('span').innerHTML.indexOf(' '));
-        currentLikeCount = parseInt(currentLikeCount);
+        currentLikeCount = element.querySelector('span').innerHTML.substr(0, element.querySelector('span').innerHTML.indexOf(' '))
+        currentLikeCount = parseInt(currentLikeCount)
 
         if (hasLiked) {
-            newLikeCount = currentLikeCount - 1;
-            hasLiked = false;
+            newLikeCount = currentLikeCount - 1
+            hasLiked = false
         } else {
-            newLikeCount = currentLikeCount + 1;
-            hasLiked = true;
+            newLikeCount = currentLikeCount + 1
+            hasLiked = true
         }
 
         if (newLikeCount === 1) {
-            newLikeText = "1 Like";
+            newLikeText = '1 Like'
         } else {
-            newLikeText = newLikeCount + " Likes";
+            newLikeText = newLikeCount + ' Likes'
         }
 
         // Toggle liked-status
         element.parentNode.style.pointerEvents = 'none' // Set PointerEvents to none to prevent multiple requests
-        
-        postId = element.id.replace("post_id_", "");
-        
-        element.querySelector('span').innerHTML = newLikeText;
+
+        postId = element.id.replace('post_id_', '')
+
+        element.querySelector('span').innerHTML = newLikeText
 
         if (hasLiked) {
-            element.classList.add('liked');
+            element.classList.add('liked')
         } else {
-            element.classList.remove('liked');
+            element.classList.remove('liked')
         }
 
         // Request
