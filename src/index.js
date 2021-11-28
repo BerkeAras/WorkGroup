659ba93f51e46bb46182612d6b30602af03db04c
Berke Aras <info@berkearas.de>
2021-11-28 11:48:20 +0100

    Add Sentry

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import * as Sentry from '@sentry/react'
+import { Integrations } from '@sentry/tracing'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import 'semantic-ui-css/semantic.min.css'
+
+if (process.env.REACT_APP_USE_SENTRY) {
+    Sentry.init({
+        dsn: process.env.REACT_APP_SENTRY_DSN,
+        integrations: [new Integrations.BrowserTracing()],
+
+        // Set tracesSampleRate to 1.0 to capture 100%
+        // of transactions for performance monitoring.
+        // We recommend adjusting this value in production
+        tracesSampleRate: 1.0,
+    })
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
